1cee136fb1170bc449cd7b0c193e0daa6fd30cca
yayanrw <yayanraw@gmail.com>
2022-02-09 12:05:05 +0700

    add mysql load

--- JWTAuth.WebApi/Program.cs
+++ JWTAuth.WebApi/Program.cs
@@ -1,7 +1,19 @@
+using JWTAuth.WebApi.Interface;
+using JWTAuth.WebApi.Models;
+using JWTAuth.WebApi.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddControllers();
+
+//Donot forgot to add ConnectionStrings as "dbConnection" to the appsetting.json file
+string connString = builder.Configuration.GetConnectionString("dbConnection");
+builder.Services.AddDbContext<DatabaseContext>(options =>
+options.UseMySql(connString, new MySqlServerVersion(new Version(10, 1, 40))));
+builder.Services.AddTransient<IEmployees, EmployeeRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
